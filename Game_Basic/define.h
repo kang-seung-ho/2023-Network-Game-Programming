86a8e2ac85d1353ea1ef10b8c055647f6cf650c3
Kang Eun Hyeok <givoontake@gmail.com>
2023-11-12 02:45:13 +0900

    define 모아놓은 헤더 추가

---
+++ Game_Basic/define.h
@@ -0,0 +1,10 @@
+#define POWERUP_ITEM 1
+#define SPEEDUP_ITEM 2
+#define HEAL_ITEM 3
+#define ICE_ITEM 4
+#define COIN 5
+
+#define BULLET_SIZE 10
+#define ITEM_SIZE 30
+#define PLAYER_SIZE 30
+#define OBSTACLE_SIZE 30
